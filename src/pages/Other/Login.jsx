5eeca097769c50319c942398bcd373520f9b9a79
AGBaby09 <agbotsem@gmail.com>
2024-12-30 20:09:48 +0000

    done

--- src/pages/Other/Login.jsx
+++ src/pages/Other/Login.jsx
@@ -27,10 +27,10 @@ const Login = () => {
       return;
     }
     
-    if (!agree) {
-      setError("You must agree to the terms.");
-      return;
-    }
+    // if (!agree) {
+    //   setError("You must agree to the terms.");
+    //   return;
+    // }
     
     setError(""); // Clear any previous errors
     
