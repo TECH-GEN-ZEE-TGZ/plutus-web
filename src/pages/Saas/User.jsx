4c14e51a1ba913f8164e8690496f0c9d0130ef5d
Young Plutus <malicksamah3@gmail.com>
2025-01-02 19:51:01 -0500

    added

--- src/pages/Saas/User.jsx
+++ src/pages/Saas/User.jsx
@@ -845,7 +845,7 @@ const Buy = ({ allCoins }) => {
 const Hash = ({ allCoins }) => {
   const { addNotification } = useContext(ContextVariables);
   var domain = "";
-  const apikey = "lsZGSdo9TopH5nikdSyz";
+  const apikey = import.meta.env.VITE_BACKEND_URL;
   const allowedCoins = ["btc", "ltc", "usdt", "xmr"];
 
   const [isLoading, setIsLoading] = useState(false);
