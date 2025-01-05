29ce07161adae6d5d8c7710ce44fad0193bd04f5
Young Plutus <malicksamah3@gmail.com>
2025-01-05 12:39:38 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.REACT_APP_BACKEND_URL': JSON.stringify('https://optimus-backend-49b31c7c7d3a.herokuapp.com'),
-    'process.env.REACT_APP_API_KEY': JSON.stringify('your-api-key'),
+    'process.env.REACT_APP_API_KEY': JSON.stringify('pFYuSfBn1Iw2XBlN-CAokQ'),
     'process.env.VITE_ENV': JSON.stringify('production'),
     'process.env.REACT_APP_TOKENVIEW_KEY': JSON.stringify('y3QfwtRNg1ruwQBqDzmm')
   }
