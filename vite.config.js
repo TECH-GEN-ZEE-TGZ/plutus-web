e71c58e05dd4f62c191c82f1371197c988db9ee4
Young Plutus <malicksamah3@gmail.com>
2025-01-06 21:09:24 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import nodePolyfills from 'vite-plugin-node-polyfills';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
